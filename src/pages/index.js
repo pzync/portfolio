0091628dbb7d0bae29249e904b9a9837f5ae1a85
pzync <shkhrgrv@gmail.com>
2019-10-28 16:09:48 +0530

    Edited the homepage content. Expanded on the Professional Summary.

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,41 +15,45 @@ const IndexPage = () => (
           Currently, I head Design at Juspay.
         </h3>
         <p>
-          At Juspay, I’ve led the design of BHIM app which saw transaction
-          volume worth INR 4.8 billion in Mar ’18. I also worked on designing
-          the UPI Common Library. This library is currently installed on ~80
-          million devices; which would become 300 million with inclusion in
-          WhatsApp soon. Various other Juspay products process 7 million+
-          transactions every single day.
+          At Juspay, I’ve led the design of BHIM app which sees transaction
+          volume of ~ INR 5 billion every month. I also worked on designing the
+          UPI Common Library. This library is currently installed on 80 million
+          devices; which would become 200 million with inclusion in WhatsApp
+          soon. Various other Juspay products process 7 million+ transactions
+          every single day and have Amazon, Uber, Visa, Myntra, Flipkart, Ola
+          etc as customers.
         </p>
         <p>
           Previously, I worked as Head of Product at{" "}
           <a href="https://www.nestaway.com" target="_blank">
             Nestaway
           </a>
-          , a startup funded over USD 100 million by Yuri Milner, Tiger Global
-          and Tata.
+          , a 1200-people company funded over USD 105 million by Tiger Global,
+          Yuri Milner & Tata. I also founded The Playce, one of the largest
+          coworking spaces in India that's still growing.
         </p>
         <p>
-          I recently wrote an essay titled{" "}
-          <a
-            href="https://uxdesign.cc/imperfect-organic-design-is-the-next-step-f16942ca79b2"
-            target="_blank"
-          >
-            Imperfect, organic design is the next step
-          </a>
-          , which you may find interesting. I also dabble in Generative Design
-          (using Processing) and sometimes{" "}
+          Overall, I have built and led teams both large n' small, shipped
+          massive-scale products on challenging timelines and founded a
+          successful business.
+        </p>
+        <p>
+          I also dabble in Generative Art (using Processing), prototype using
+          code and sometimes{" "}
           <a href="https://twitter.com/themeanstartup" target="_blank">
             {" "}
-            draw comics
+            draw
           </a>{" "}
           as well.
         </p>
         <p>
-          You can reach out to me on twitter at{" "}
+          Feel free to reach out on{" "}
           <a href="http://twitter.com/ShekharGurav" target="_blank">
-            @ShekharGurav
+            twitter
+          </a>{" "}
+          &{" "}
+          <a href="http://in.linkedin.com/in/ShekharGurav" target="_blank">
+            linkedIn
           </a>
           .
         </p>
